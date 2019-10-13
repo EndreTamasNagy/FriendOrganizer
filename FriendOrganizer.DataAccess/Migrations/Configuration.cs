617102c2e31f89ad85881c65c99d8d483ae50c52
Endre <tamas.nagy.endre@gmail.com>
2019-10-13 23:15:16 +0200

    Add project files.

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+﻿namespace FriendOrganizer.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Model.Friend { FirstName = "Endre", LastName = "Nagy" },
+                new Model.Friend { FirstName = "Csilla", LastName = "Böcskei" },
+                new Model.Friend { FirstName = "Zsófia", LastName = "Simándi" },
+                new Model.Friend { FirstName = "Szabolcs", LastName = "Orbán" }
+                );
+        }
+    }
+}
---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,32 @@
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        private Func<FriendOrganizerDbContext> _contextCreator;
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
+        public async Task<List<Friend>> GetAllAsync()
+        {
+            
+            using (var ctx = _contextCreator())
+            {
+                return await ctx.Friends.AsNoTracking().ToListAsync();
+            } 
+            //// TODO: Load data from real database
+            //yield return new Friend { FirstName = "Endre", LastName = "Nagy" };
+            //yield return new Friend { FirstName = "Csilla", LastName = "Böcskei" };
+            //yield return new Friend { FirstName = "Zsófia", LastName = "Simándi" };
+            //yield return new Friend { FirstName = "Szabolcs", LastName = "Orbán" };
+        }
+    }
+}
